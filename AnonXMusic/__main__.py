dc07b3bb04d0357875bdee3b1bcc06f13586b760
ZEOOXX44 <159913416+ZEOOX9210@users.noreply.github.com>
2024-05-06 07:48:53 +0530

    Update __main__.py
    Signed-off-by: ZEOOXX44 <159913416+ZEOOX9210@users.noreply.github.com>

--- AnonXMusic/__main__.py
+++ AnonXMusic/__main__.py
@@ -55,7 +55,7 @@ async def init():
     await idle()
     await app.stop()
     await userbot.stop()
-    LOGGER("AnonXMusic").info("ʀᴇᴄʀᴇᴀᴛɪᴏɴ  ᴍᴜꜱɪᴄ Music Bot...")
+    LOGGER("AnonXMusic").info("Stopping AnonX Music Bot...")
 
 
 if __name__ == "__main__":
